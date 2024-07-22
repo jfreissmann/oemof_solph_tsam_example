76742f9a18947e9cc60a123f42b1f3c62d487ef1
Jonas Freißmann <j.freissmann@web.de>
2024-07-22 14:45:05 +0200

    Compare kmeans and hierarchical clustering.

--- src/comp_agg_methods.py
+++ src/comp_agg_methods.py
@@ -19,74 +19,67 @@ opex_total_base, heat_prod_shares_base, heat_prod_total_base = calc_key_params(
 noTypicalPeriods = 10
 hoursPerPeriod = 120
 
-aggregation, agg_data = preprocessing(
-    noTypicalPeriods=noTypicalPeriods, hoursPerPeriod=hoursPerPeriod,
-    data=ts_in
-)
-results = optimization(agg_data=agg_data)
-desagg_data = postprocessing(
-    results=results, agg_data=agg_data, aggregation=aggregation
-)
+kwargses = {
+    'K means w/o extreme periods': {
+        'clusterMethod': 'k_means'
+    },
+    'K means w/ extreme periods': {
+        'clusterMethod': 'k_means',
+        'extremePeriodMethod': 'new_cluster_center',
+        'addPeakMin': ['heat'],
+        'addPeakMax': ['heat']
+    },
+    'Hierarchical w/o extreme periods': {
+        'clusterMethod': 'hierarchical'
+    },
+    'Hierarchical w/ extreme periods': {
+        'clusterMethod': 'hierarchical',
+        'extremePeriodMethod': 'new_cluster_center',
+        'addPeakMin': ['heat'],
+        'addPeakMax': ['heat']
+    }
+}
 
-aggregation_extreme, agg_data_extreme = preprocessing(
-    noTypicalPeriods=noTypicalPeriods, hoursPerPeriod=hoursPerPeriod,
-    data=ts_in, clusterMethod='hierarchical'
-)
-results_extreme = optimization(agg_data=agg_data_extreme)
-desagg_data_extreme = postprocessing(
-    results=results_extreme, agg_data=agg_data_extreme,
-    aggregation=aggregation_extreme
-)
-# aggregation_extreme, agg_data_extreme = preprocessing(
-#     noTypicalPeriods=noTypicalPeriods, hoursPerPeriod=hoursPerPeriod,
-#     data=ts_in, extremePeriodMethod='new_cluster_center'
-# )
-# results_extreme = optimization(agg_data=agg_data_extreme)
-# desagg_data_extreme = postprocessing(
-#     results=results_extreme, agg_data=agg_data_extreme,
-#     aggregation=aggregation_extreme
-# )
-
-fig, axs = plt.subplots(1, 2, sharey='col', figsize=(14, 6))
+fig, axs = plt.subplots(2, 2, sharex='row', sharey='col', figsize=(13, 13))
 
 heatcol = 'heat bus to heat demand'
 
-axs[0].plot(
-    (0, unitdata_base[heatcol].max()*1.05),
-    (0, unitdata_base[heatcol].max()*1.05),
-    color='k'
-)
-axs[0].scatter(unitdata_base[heatcol], desagg_data[heatcol])
-axs[1].plot(
-    (0, unitdata_base[heatcol].max()*1.05),
-    (0, unitdata_base[heatcol].max()*1.05),
-    color='k'
-)
-axs[1].scatter(unitdata_base[heatcol], desagg_data_extreme[heatcol])
+fig.suptitle(f'{noTypicalPeriods = }, {hoursPerPeriod = }')
 
-corr = []
-corr += [
-    np.corrcoef(
+for i, (name, kwargs) in enumerate(kwargses.items()):
+    aggregation, agg_data = preprocessing(
+        noTypicalPeriods=noTypicalPeriods, hoursPerPeriod=hoursPerPeriod,
+        data=ts_in, **kwargs
+    )
+    results = optimization(agg_data=agg_data)
+    desagg_data = postprocessing(
+        results=results, agg_data=agg_data, aggregation=aggregation
+    )
+
+    corr = np.corrcoef(
         unitdata_base[heatcol].to_numpy(), desagg_data[heatcol].to_numpy()
     )
-]
-corr += [
-    np.corrcoef(
-        unitdata_base[heatcol].to_numpy(),
-        desagg_data_extreme[heatcol].to_numpy()
+
+    row = int(i % 2)
+    col = int(np.floor(i / 2))
+
+    axs[row, col].plot(
+        (0, unitdata_base[heatcol].max()*1.05),
+        (0, unitdata_base[heatcol].max()*1.05),
+        color='k'
     )
-]
+    axs[row, col].scatter(unitdata_base[heatcol], desagg_data[heatcol])
 
-for i, ax in enumerate(axs):
-    ax.set_axisbelow(True)
-    ax.grid()
-    ax.set_xlim(left=0, right=unitdata_base[heatcol].max()*1.05)
-    ax.set_ylim(bottom=0, top=unitdata_base[heatcol].max()*1.05)
-    ax.set_xlabel('Real heat load in MWh')
-    ax.set_ylabel('Aggregated heat load in MWh')
+    axs[row, col].set_axisbelow(True)
+    axs[row, col].grid()
+    axs[row, col].set_xlim(left=0, right=unitdata_base[heatcol].max()*1.05)
+    axs[row, col].set_ylim(bottom=0, top=unitdata_base[heatcol].max()*1.05)
+    axs[row, col].set_xlabel('Real heat load in MWh')
+    axs[row, col].set_ylabel('Aggregated heat load in MWh')
+    axs[row, col].set_title(name)
 
-    ax.annotate(
-        f'R = {corr[i][0, 1]:.4f}',
+    axs[row, col].annotate(
+        f'R = {corr[0, 1]:.4f}',
         (0.02, 0.95), xycoords='axes fraction',
         bbox=dict(boxstyle='round', fc='#f1f1f1')
     )
